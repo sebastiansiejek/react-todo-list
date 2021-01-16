19cedbe7433f838e3b083686f1e92a66f9259053
Sebastian Siejek <contact@sebastiansiejek.dev>
2021-01-16 13:05:33 +0100

    chore: change task with tasks

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,12 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit'
-import task, { initialState as taskState } from './slices/tasksSlice'
+import tasks, { initialState as taskState } from './slices/tasksSlice'
 
 export interface IStore {
-  task: typeof taskState
+  tasks: typeof taskState
 }
 
 export const rootReducer = combineReducers({
-  task
+  tasks
 })
 
 export default configureStore({
