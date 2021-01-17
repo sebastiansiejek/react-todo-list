2605bcb1c93265e81efe88e523ecc821a3e3ea57
Sebastian Siejek <contact@sebastiansiejek.dev>
2021-01-17 10:00:56 +0100

    feat: add toggleComplete action and checkbox

--- src/components/smarts/ListItem/ListItem.tsx
+++ src/components/smarts/ListItem/ListItem.tsx
@@ -1,22 +1,25 @@
 import styled from 'styled-components'
 import { ITask } from 'types/ITasks/Itasks'
-import { removeTask } from 'store/slices/tasksSlice'
+import { removeTask, toggleComplete } from 'store/slices/tasksSlice'
 import { useDispatch } from 'react-redux'
 
 const ListItemStyled = styled.li`
   list-style: none;
   display: flex;
 
-  input {
+  input[type='text'] {
     padding: 0.6rem;
-    border: 0.1rem solid lightgray;
     flex: 1;
   }
 
+  input[type='checkbox'] {
+    align-self: center;
+  }
+
   &:not(:last-child) {
-    input {
-      border-bottom: none;
-    }
+    padding-bottom: 0.6rem;
+    margin-bottom: 0.6rem;
+    border-bottom: 0.1rem solid lightgray;
   }
 `
 
@@ -29,7 +32,15 @@ const ListItem: React.FC<IProps> = ({ task }) => {
 
   return (
     <ListItemStyled>
-      <input readOnly value={task.task} />
+      <input
+        type="checkbox"
+        onChange={e =>
+          dispatch(
+            toggleComplete({ id: task.id, is_completed: e.target.checked })
+          )
+        }
+      />
+      <input type="text" readOnly value={task.task} />
       <button onClick={() => dispatch(removeTask(task.id))}>Remove</button>
     </ListItemStyled>
   )
--- src/store/slices/tasksSlice.ts
+++ src/store/slices/tasksSlice.ts
@@ -18,11 +18,19 @@ const tasksSlice = createSlice({
     },
     removeTask(state: ITasksState, action: PayloadAction<string>) {
       state.tasks = state.tasks.filter(task => task.id !== action.payload)
+    },
+    toggleComplete(
+      state: ITasksState,
+      action: PayloadAction<{ id: string; is_completed: boolean }>
+    ) {
+      const { id, is_completed } = action.payload
+      const task = state.tasks.find(task => task.id === id)
+      if (task) task.is_completed = is_completed
     }
   }
 })
 
 export const getTasks = (state: ITasksState) => state.tasks
 
-export const { addTask, removeTask } = tasksSlice.actions
+export const { addTask, removeTask, toggleComplete } = tasksSlice.actions
 export default tasksSlice.reducer
